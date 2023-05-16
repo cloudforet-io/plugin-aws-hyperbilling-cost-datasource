8539fdfc8ef30e05932494c78cf5fb892074f7b5
jiyungSong <bluese@megazone.com>
2023-05-16 19:53:23 +0900

    refactor: default None

--- src/spaceone/cost_analysis/model/cost_model.py
+++ src/spaceone/cost_analysis/model/cost_model.py
@@ -13,7 +13,7 @@ class Cost(Model):
     product = StringType()
     account = StringType(required=True)
     usage_type = StringType()
-    usage_unit = StringType(default=None, null=True)
+    usage_unit = StringType(default=None)
     billed_at = DateTimeType(required=True)
     additional_info = DictType(StringType, default={})
     tags = DictType(StringType, default={})
