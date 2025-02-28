17179e749ea416518199a53b324aeaeb305c8990
ImMin5 <mino@megazone.com>
2025-02-28 20:12:17 +0900

    build: modify spaceone-api version `1.12.19.5`
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/spaceone/cost_analysis/info/cost_info.py
+++ src/spaceone/cost_analysis/info/cost_info.py
@@ -4,7 +4,7 @@ from spaceone.api.cost_analysis.plugin import cost_pb2
 from spaceone.core.pygrpc.message_type import *
 from spaceone.core import utils
 
-__all__ = ['CostInfo', 'CostsInfo']
+__all__ = ["CostInfo", "CostsInfo"]
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -12,30 +12,38 @@ _LOGGER = logging.getLogger(__name__)
 def CostInfo(cost_data):
     try:
         info = {
-            'cost': cost_data['cost'],
-            'usd_cost': cost_data['cost'],
-            'currency': cost_data['currency'],
-            'usage_quantity': cost_data.get('usage_quantity'),
-            'provider': cost_data.get('provider'),
-            'region_code': cost_data.get('region_code'),
-            'category': cost_data.get('category'),
-            'product': cost_data.get('product'),
-            'account': cost_data.get('account'),
-            'usage_type': cost_data.get('usage_type'),
-            'usage_unit': cost_data.get('usage_unit'),
-            'resource': cost_data.get('resource'),
-            'tags': change_struct_type(cost_data['tags']) if 'tags' in cost_data else None,
-            'additional_info': change_struct_type(cost_data['additional_info']) if 'additional_info' in cost_data else None,
-            'billed_at': utils.datetime_to_iso8601(cost_data['billed_at'])
+            "cost": cost_data["cost"],
+            "usd_cost": cost_data["cost"],
+            "currency": cost_data["currency"],
+            "usage_quantity": cost_data.get("usage_quantity"),
+            "provider": cost_data.get("provider"),
+            "region_code": cost_data.get("region_code"),
+            "category": cost_data.get("category"),
+            "product": cost_data.get("product"),
+            "account": cost_data.get("account"),
+            "usage_type": cost_data.get("usage_type"),
+            "usage_unit": cost_data.get("usage_unit"),
+            "resource": cost_data.get("resource"),
+            "tags": (
+                change_struct_type(cost_data["tags"]) if "tags" in cost_data else None
+            ),
+            "additional_info": (
+                change_struct_type(cost_data["additional_info"])
+                if "additional_info" in cost_data
+                else None
+            ),
+            "billed_at": utils.datetime_to_iso8601(cost_data["billed_at"]),
         }
 
         return cost_pb2.CostInfo(**info)
 
     except Exception as e:
-        _LOGGER.debug(f'[CostInfo] cost data: {cost_data}')
-        _LOGGER.debug(f'[CostInfo] error reason: {e}', exc_info=True)
+        _LOGGER.debug(f"[CostInfo] cost data: {cost_data}")
+        _LOGGER.debug(f"[CostInfo] error reason: {e}", exc_info=True)
         raise e
 
 
 def CostsInfo(costs_data, **kwargs):
-    return cost_pb2.CostsInfo(results=list(map(functools.partial(CostInfo, **kwargs), costs_data)))
+    return cost_pb2.CostsInfo(
+        results=list(map(functools.partial(CostInfo, **kwargs), costs_data))
+    )
